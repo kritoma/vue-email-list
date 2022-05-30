6052569dfce6c1bcc5688609873cb43b13004e71
kritoma <toma.kristian97@gmail.com>
2022-05-30 14:38:30 +0200

    bonus

--- main.js
+++ main.js
@@ -4,15 +4,14 @@ const app = new Vue({
         emails: [],
     },
     created() {
+        const email = [];
         for (let i = 0; i < 10; i++) {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
             .then((elm) => {
                 console.log(elm.data.response);
                 this.emails.push(elm.data.response);
-                    
-                
             })
-        }
-       
+        };
+        
     }
 })
